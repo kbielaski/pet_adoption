d5a0e635250bd931eb637c4621dd9f7b630c8994
Yuyu <44049122+janny5005@users.noreply.github.com>
2018-11-29 15:43:26 -0800

    Updated Adotion Application

--- adoption_application.js
+++ adoption_application.js
@@ -9,68 +9,166 @@ $( document ).ready(function() {
 	var reasons, questions;
 
 	$(".back").click(function(){
-		window.location.href='search_results.html';
+		window.location.href='./search_results.html';
+	});
+	$(".pic-scaled").click(function(){
+		window.location.href='./index.html';
 	});
 
-	$(".next").click(function(){
-		if(animating) return false;
-		animating = true;
-		
-		current_fs = $(this).parent();
-		next_fs = $(this).parent().next();
-		
-		//activate next step on progressbar using the index of next_fs
-		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-		
-		//show the next fieldset
-		next_fs.show(); 
-		//hide the current fieldset with style
-		current_fs.animate({opacity: 0}, {
-			step: function(now, mx) {
-				//as the opacity of current_fs reduces to 0 - stored in "now"
-				//1. scale current_fs down to 80%
-				scale = 1 - (1 - now) * 0.2;
-				//2. bring next_fs from the right(50%)
-				left = (now * 50)+"%";
-				//3. increase opacity of next_fs to 1 as it moves in
-				opacity = 1 - now;
-				current_fs.css({
-	        'transform': 'scale('+scale+')',
-	        'position': 'absolute'
-	      });
-				next_fs.css({'left': left, 'opacity': opacity});
-			}, 
-			duration: 800, 
-			complete: function(){
-				current_fs.hide();
-				animating = false;
-			}, 
-			//this comes from the custom easing plugin
-			easing: 'easeInOutBack'
-		});
+/*
+	//autocomplete
+    $( function() {
+      
+      stateList = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",
+      			   "HI","ID","IL","IN","IA","KS","KY","LA","ME","MD",
+      			   "MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ",
+      			   "NM","NY","NC","ND","OH","OK","OR","PA","RI","SC",
+      			   "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
+      //autocomplete
+      $( "#state" ).autocomplete({
+        //at least two character
+        minLength:1,
+        //find the match in the capital list
+        source: stateList,
+        // while select a value from the drop down menu
+        select: function( event, ui ) {
+          // set input equal to the selection from the list
+          var input = ui.item.value;
+          //autocomlete the fill with the selected value, and check the answer
+          $( "#state" ).text(input);
+          
+          return false;
+        }
+      });
 
+    });
+*/
+	$(".first_page").click(function(){
+
+		var isValid = true;
 		firstName = $('#firstName').val();
+		if(firstName=="") isValid = false;
 		lastName = $('#lastName').val();
+		if(lastName=="") isValid = false;
 		email = $('#email').val();
+		if(email=="") isValid = false;
 		phone = $('#phone').val();
+		if(phone=="") isValid = false;
 		address = $('#address').val();
+		if(address=="") isValid = false;
 		city = $('#city').val();
+		if(city=="") isValid = false;
 		state = $('#state').val();
+		if(state=="") isValid = false;
 		zipcode = $('#zipcode').val();
+		if(zipcode=="") isValid = false;
 		interviewForm = $( "#interviewForm option:selected" ).text();
+		if(interviewForm==" -- select one -- ") isValid = false;
 		interviewDate = $('#interviewDate').val();
+		if(interviewDate=="") isValid = false;
 		interviewTime = $( "#interviewTime option:selected" ).text();
-		reasons = $('#reasons').val();
-		questions = $('#questions').val();
-		$('#Name').html(firstName+" "+lastName);
-		$('#Email').html(email);
-		$('#Phone').html(phone);
-		$('#Address').html(address+", "+city+", "+state+", "+zipcode);
-		$('#Interview').html(interviewDate+"  "+interviewTime+"  "+interviewForm);
-		$('#Reasons').html(reasons);
-		$('#Questions').html(questions);
+		if(interviewTime==" -- select a time slot-- ") isValid = false;
+		
+
+
+		if (isValid==true){
+			$('#Name').html(firstName+" "+lastName);
+			$('#Email').html(email);
+			$('#Phone').html(phone);
+			$('#Address').html(address+", "+city+", "+state+", "+zipcode);
+			$('#Interview').html(interviewDate+"  "+interviewTime+"   "+interviewForm);
+
+			if(animating) return false;
+			animating = true;
+			
+			current_fs = $(this).parent();
+			next_fs = $(this).parent().next();
+			
+			//activate next step on progressbar using the index of next_fs
+			$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+			//show the next fieldset
+			next_fs.show(); 
+			//hide the current fieldset with style
+			current_fs.animate({opacity: 0}, {
+				step: function(now, mx) {
+					//as the opacity of current_fs reduces to 0 - stored in "now"
+					//1. scale current_fs down to 80%
+					scale = 1 - (1 - now) * 0.2;
+					//2. bring next_fs from the right(50%)
+					left = (now * 50)+"%";
+					//3. increase opacity of next_fs to 1 as it moves in
+					opacity = 1 - now;
+					current_fs.css({
+		        'transform': 'scale('+scale+')',
+		        'position': 'absolute'
+		      });
+					next_fs.css({'left': left, 'opacity': opacity});
+				}, 
+				duration: 800, 
+				complete: function(){
+					current_fs.hide();
+					animating = false;
+				}, 
+				//this comes from the custom easing plugin
+				easing: 'easeInOutBack'
+			});
+		}
+
+		
+
+		
 	});
 
+	$(".second_page").click(function(){
+		var isValid = true;
+
+
+		reasons = $('#reasons').val();
+		if(reasons=="") isValid = false;
+		questions = $('#questions').val();
+		
+
+		if(isValid==true){
+			$('#Reasons').html(reasons);
+			$('#Questions').html(questions);
+
+			if(animating) return false;
+			animating = true;
+			
+			current_fs = $(this).parent();
+			next_fs = $(this).parent().next();
+			
+			//activate next step on progressbar using the index of next_fs
+			$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+			
+			//show the next fieldset
+			next_fs.show(); 
+			//hide the current fieldset with style
+			current_fs.animate({opacity: 0}, {
+				step: function(now, mx) {
+					//as the opacity of current_fs reduces to 0 - stored in "now"
+					//1. scale current_fs down to 80%
+					scale = 1 - (1 - now) * 0.2;
+					//2. bring next_fs from the right(50%)
+					left = (now * 50)+"%";
+					//3. increase opacity of next_fs to 1 as it moves in
+					opacity = 1 - now;
+					current_fs.css({
+		        'transform': 'scale('+scale+')',
+		        'position': 'absolute'
+		      });
+					next_fs.css({'left': left, 'opacity': opacity});
+				}, 
+				duration: 800, 
+				complete: function(){
+					current_fs.hide();
+					animating = false;
+				}, 
+				//this comes from the custom easing plugin
+				easing: 'easeInOutBack'
+			});
+		}
+	});
 
 	$(".previous").click(function(){
 		if(animating) return false;
